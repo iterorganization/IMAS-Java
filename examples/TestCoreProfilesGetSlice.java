a5f8c646b7ef181beaf21d406f4dd45997982804
Olivier Hoenen <Olivier.Hoenen@iter.org>
2024-02-20 09:39:15 +0100

    Fix: NullPointerException in examples after MDS+ correction (IMAS-4957)

--- examples/TestCoreProfilesGetSlice.java
+++ examples/TestCoreProfilesGetSlice.java
@@ -26,8 +26,10 @@ class TestCoreProfilesGetSlice {
             System.out.println("time = " + time);
             System.out.println("ids_properties.homogeneous: " + ids.ids_properties.homogeneous_time);
             System.out.println("ids_properties.comment:     " + ids.ids_properties.comment);
-            System.out.println("size of profiles_1d:        " + ids.profiles_1d.length);
-            System.out.println("profiles_1d.time :          " + ids.profiles_1d[0].time);
+	    if (ids.profiles_1d != null) {
+		System.out.println("size of profiles_1d:        " + ids.profiles_1d.length);
+		System.out.println("profiles_1d.time :          " + ids.profiles_1d[0].time);
+	    }
             System.out.println("main IDS time :             " + ids.time);
             System.out.println("IP            :             " + ids.global_quantities.ip);
             imas.close(idx);
