c08f93c4bbb5e33bbe5113b6fa04cd370f399417
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2024-04-21 22:31:13 +0200

    Fix test script

--- examples/new-examples/build-and-run.sh
+++ examples/new-examples/build-and-run.sh
@@ -4,4 +4,4 @@ docker run --rm -it \
     -v `pwd`/imasdb:/root/public/imasdb \
     -e CLASSPATH=`pwd` \
     gitlab.eufus.psnc.pl:5050/containerization/imas/imas-installer/al:DD-3.39.0_AL-5.0.0-UDA-2.7.4-UDA-PLUGIN-1.4.0 \
-    bash -c "module load IMAS && javac examples/UserStory1.java && java examples.UserStory1"
+    bash -c "module load IMAS && javac examples/example001_OpenDatabase.java && java examples.example001_OpenDatabase"
