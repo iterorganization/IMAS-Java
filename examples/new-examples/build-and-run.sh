e6bb39a88efb452e215d697326043830851d452e
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2024-04-22 11:01:10 +0200

    Use AL 5.1.1

--- examples/new-examples/build-and-run.sh
+++ examples/new-examples/build-and-run.sh
@@ -3,5 +3,5 @@ docker run --rm -it \
     -v `pwd`:`pwd` \
     -v `pwd`/imasdb:/root/public/imasdb \
     -e CLASSPATH=`pwd` \
-    gitlab.eufus.psnc.pl:5050/containerization/imas/imas-installer/al:DD-3.39.0_AL-5.0.0-UDA-2.7.4-UDA-PLUGIN-1.4.0 \
+    gitlab.eufus.psnc.pl:5050/containerization/imas/imas-installer/al:DD-3.39.0_AL-5.1.1-UDA-2.7.4-UDA-PLUGIN-1.4.0 \
     bash -c "module load IMAS && javac examples/example001_OpenDatabase.java && java examples.example001_OpenDatabase"
