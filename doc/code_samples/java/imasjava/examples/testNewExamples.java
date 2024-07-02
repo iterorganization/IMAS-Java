e6b0b94533589ee0a27626d528734d382534b593
Aleksander Wiszniowski <awiszniowski@man.poznan.pl>
2024-07-02 14:50:35 +0200

    moved java examples to new location

--- doc/code_samples/java/imasjava/examples/testNewExamples.java
+++ doc/code_samples/java/imasjava/examples/testNewExamples.java
@@ -4,10 +4,12 @@ import example002_fillingDataInIDS;
 public class testNewExamples {
     public static void main(String[] args) throws Exception {
         try {
+            
             System.out.println("Running example case");
-            example002_fillingDataInIDS x = new example002_fillingDataInIDS();
-            x.creatingCompletlyNewIDS();
-            x.defaultValuesAndAosOperations();
+            example002_fillingDataInIDS.creatingCompletlyNewIDS();
+            example002_fillingDataInIDS.defaultValuesAndAosOperations();
+            example002_fillingDataInIDS.copyingAndValidatingIDS();
+
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         }
