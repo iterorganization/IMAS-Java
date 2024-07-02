38878169c98ce4ba95f4123acedcf4ee1088d3be
Aleksander Wiszniowski <awiszniowski@man.poznan.pl>
2024-07-02 14:55:57 +0200

    moved story no 4 examples to new location

--- doc/code_samples/java/imasjava/examples/testNewExamples.java
+++ doc/code_samples/java/imasjava/examples/testNewExamples.java
@@ -4,12 +4,11 @@ import example004_readDataFromEntry;
 public class testNewExamples {
     public static void main(String[] args) throws Exception {
         try {
+            
             System.out.println("Running example case");
-
-            example004_readDataFromEntry example004 = new example004_readDataFromEntry();
-            example004.readEntireIDS();
-            example004.readSlice();
-            example004.pratialGet();
+            example004_readDataFromEntry.readEntireIDS();
+            example004_readDataFromEntry.readSlice();
+            example004_readDataFromEntry.pratialGet();
 
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
