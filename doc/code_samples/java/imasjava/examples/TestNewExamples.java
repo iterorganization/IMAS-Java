13a83b0e5712ba55eaddb3ed0730a0686711b850
Aleksander Wiszniowski <awiszniowski@man.poznan.pl>
2024-07-12 09:30:41 +0200

    changes in import

--- doc/code_samples/java/imasjava/examples/TestNewExamples.java
+++ doc/code_samples/java/imasjava/examples/TestNewExamples.java
@@ -1,13 +1,13 @@
 package imasjava.examples;
-import Example001_OpenDatabase;
+import imasjava.examples.Example001_OpenDatabase;
 
 public class TestNewExamples {
     public static void main(String[] args) throws Exception {
         try {
             System.out.println("Running OpenDatabase example case");
-            example001_OpenDatabase.createDBEntryLegacy();
-            example001_OpenDatabase.openDBEntryURI();
-            example001_OpenDatabase.createDBEntryURIwithPath();
+            Example001_OpenDatabase.createDBEntryLegacy();
+            Example001_OpenDatabase.openDBEntryURI();
+            Example001_OpenDatabase.createDBEntryURIwithPath();
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         }
