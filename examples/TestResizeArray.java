a72e4294502380e4137c6eda62b3a80fdee8adb3
marood <deepakkumar.maroo@iter.org>
2024-08-26 17:01:28 +0200

    modified example to support both DD3 and DD4

--- examples/TestResizeArray.java
+++ examples/TestResizeArray.java
@@ -1,7 +1,7 @@
 import java.lang.*;
 
 import imasjava.*;
-import imasjava.ids.magnetics_IDSBase.b_field_phi_probeClass;
+import imasjava.ids.magnetics_IDSBase.b_field_pol_probeClass;
 import imasjava.ids.magnetics_IDSBase.flux_loopClass;
 import imasjava.wrapper.LowLevel;
 
@@ -15,52 +15,43 @@ class TestResizeArray {
         ids.time = new Vect1DDouble(10);
         ids.time.setArray(time, 10);
         ids.flux_loop = new imas.magnetics.flux_loopClass[10];
-        System.out.println("### Testing HOMOGENEOUS flux_loop size..." + ids.flux_loop.length);
+        System.out.println("### HOMOGENEOUS flux_loop size..." + ids.flux_loop.length);
         for (int i = 0; i < ids.flux_loop.length; i++) {
             ids.flux_loop[i] = new imas.magnetics.flux_loopClass();
             ids.flux_loop[i].indices_differential = new Vect1DInt(3);
         }
-        System.out.println("### Before resize ids.flux_loop[0]: " + ids.flux_loop[0]);
+        System.out.println("### Before resizing magnetics.flux_loop[0]: " + ids.flux_loop[0]);
         ids.time = new Vect1DDouble(15);
         ids.time.setArray(time, 15);
         ids.flux_loop = (flux_loopClass[]) imasjava.utilities.ImasReflection.getResizedClassObject(ids.flux_loop, 15);
-        System.out.println("### Testing resize flux_loop array resize: " + ids.flux_loop.length);
+        System.out.println("### Resized flux_loop array resize: " + ids.flux_loop.length);
         for (int i = 10; i < ids.flux_loop.length; i++) {
             ids.flux_loop[i] = new imas.magnetics.flux_loopClass();
             ids.flux_loop[i].indices_differential = new Vect1DInt(3);
         }
-        System.out.println("### After resize ids.flux_loop[0]: " + ids.flux_loop[0]);
-        System.out.println("### After resize ids.flux_loop[14]: " + ids.flux_loop[14]);
+        System.out.println("### After resize magnetics.flux_loop[0]: " + ids.flux_loop[0]);
+        System.out.println("### After resize magnetics.flux_loop[14]: " + ids.flux_loop[14]);
 
         System.out.println("");
 
-        System.out.println("### Testing b_field_phi_probe resize...");
-        ids.b_field_phi_probe = new imas.magnetics.b_field_phi_probeClass[13];
+        System.out.println("### Testing b_field_pol_probe resize...");
         ids.b_field_pol_probe = new imas.magnetics.b_field_pol_probeClass[13];
-        System.out.println("### Testing b_field_phi_probe size: " + ids.b_field_phi_probe.length);
+        System.out.println("### magnetics b_field_pol_probe size: " + ids.b_field_pol_probe.length);
         for (int i = 0; i < 13; i++) {
-            ids.b_field_phi_probe[i] = new imas.magnetics.b_field_phi_probeClass();
-            ids.b_field_phi_probe[i].field.data = new Vect1DDouble(9);
-            ids.b_field_phi_probe[i].field.time = new Vect1DDouble(9);
             ids.b_field_pol_probe[i] = new imas.magnetics.b_field_pol_probeClass();
             ids.b_field_pol_probe[i].field.data = new Vect1DDouble(9);
             ids.b_field_pol_probe[i].field.time = new Vect1DDouble(9);
         }
-        System.out.println("### Before resize ids.b_field_phi_probe[0]: " + ids.b_field_phi_probe[0]);
-
-        ids.b_field_phi_probe = (b_field_phi_probeClass[]) imasjava.utilities.ImasReflection.getResizedClassObject(ids.b_field_phi_probe, 20);
-        ids.b_field_pol_probe = new imas.magnetics.b_field_pol_probeClass[ids.b_field_phi_probe.length];
-        for (int i = 13; i < ids.b_field_phi_probe.length; i++) {
-            ids.b_field_phi_probe[i] = new imas.magnetics.b_field_phi_probeClass();
-            ids.b_field_phi_probe[i].field.data = new Vect1DDouble(9);
-            ids.b_field_phi_probe[i].field.time = new Vect1DDouble(9);
+        System.out.println("### Before resizing magnetics.b_field_pol_probe[0]: " + ids.b_field_pol_probe[0]);
+        ids.b_field_pol_probe = (b_field_pol_probeClass[]) imasjava.utilities.ImasReflection.getResizedClassObject(ids.b_field_pol_probe, 20);
+	System.out.println("### Resized magnetics.b_field_pol_probe array resize: " + ids.b_field_pol_probe.length);
+        for (int i = 13; i < ids.b_field_pol_probe.length; i++) {
             ids.b_field_pol_probe[i] = new imas.magnetics.b_field_pol_probeClass();
             ids.b_field_pol_probe[i].field.data = new Vect1DDouble(9);
             ids.b_field_pol_probe[i].field.time = new Vect1DDouble(9);
         }
-        System.out.println("### Testing b_field_phi_probe resize: " + ids.b_field_phi_probe.length);
-        System.out.println("### After resize ids.b_field_phi_probe[0]: " + ids.b_field_phi_probe[0]);
-        System.out.println("### After resize ids.b_field_phi_probe[19]: " + ids.b_field_phi_probe[19]);
+        System.out.println("### After resize magnetics.b_field_pol_probe[0]: " + ids.b_field_pol_probe[0]);
+        System.out.println("### After resize magnetics.b_field_tor_probe[19]: " + ids.b_field_pol_probe[19]);
 
         System.out.println("");
     }
