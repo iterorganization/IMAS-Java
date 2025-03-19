b54e688bc7e7b42c6006892adcc6d06b93fc9ee6
sawantp1 <prasadparshuram.sawantdesai@iter.org>
2025-03-19 17:31:52 +0100

    get cmake setting from environment variables

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -111,30 +111,30 @@ rm -rf build
 
 # CMake configuration:
 CMAKE_ARGS=(
-    -D "CMAKE_INSTALL_PREFIX=$(pwd)/test-install/"
-    # Enable all backends
-    -D AL_BACKEND_HDF5=ON
-    -D AL_BACKEND_MDSPLUS=ON
-    -D AL_BACKEND_UDA=ON
-    # Build MDSplus models
-    -D AL_BUILD_MDSPLUS_MODELS=ON
-    # Download dependencies from HTTPS (using an access token):
-    -D AL_DOWNLOAD_DEPENDENCIES=ON
-    -D AL_CORE_GIT_REPOSITORY=https://git.iter.org/scm/imas/al-core.git
-    -D AL_PLUGINS_GIT_REPOSITORY=https://git.iter.org/scm/imas/al-plugins.git
-    -D DD_GIT_REPOSITORY=https://git.iter.org/scm/imas/data-dictionary.git
-    # DD version: can be set with DD_VERSION env variable, otherwise use latest main
-    -D DD_VERSION=${DD_VERSION:-main}
-    # AL-Core version: can be set with AL_CORE_VERSION env variable, otherwise use latest main
-    -D AL_CORE_VERSION=${AL_CORE_VERSION:-main}
-    # HLI options
-    -D AL_EXAMPLES=ON
-    -D AL_TESTS=ON
-    -D AL_PLUGINS=ON
-    # Build documentation
-    -D AL_HLI_DOCS=ON
-    # Work around Boost linker issues on 2020b toolchain
-    -D Boost_NO_BOOST_CMAKE=ON
+  -D "CMAKE_INSTALL_PREFIX=$(pwd)/test-install/"
+  # Enable all backends
+  -D AL_BACKEND_HDF5=${AL_BACKEND_HDF5:-ON}
+  -D AL_BACKEND_MDSPLUS=${AL_BACKEND_MDSPLUS:-ON}
+  -D AL_BACKEND_UDA=${AL_BACKEND_UDA:-ON}
+  # Build MDSplus models
+  -D AL_BUILD_MDSPLUS_MODELS=${AL_BUILD_MDSPLUS_MODELS:-ON}
+  # Download dependencies from HTTPS (using an access token):
+  -D AL_DOWNLOAD_DEPENDENCIES=${AL_DOWNLOAD_DEPENDENCIES:-ON}
+  -D AL_CORE_GIT_REPOSITORY=${AL_CORE_GIT_REPOSITORY:-https://git.iter.org/scm/imas/al-core.git}
+  -D AL_PLUGINS_GIT_REPOSITORY=${AL_PLUGINS_GIT_REPOSITORY:-https://git.iter.org/scm/imas/al-plugins.git}
+  -D DD_GIT_REPOSITORY=${DD_GIT_REPOSITORY:-https://git.iter.org/scm/imas/data-dictionary.git}
+  # DD version: can be set with DD_VERSION env variable, otherwise use latest main
+  -D DD_VERSION=${DD_VERSION:-main}
+  # AL-Core version: can be set with AL_CORE_VERSION env variable, otherwise use latest main
+  -D AL_CORE_VERSION=${AL_CORE_VERSION:-main}
+  # HLI options
+  -D AL_EXAMPLES=${AL_EXAMPLES:-ON}
+  -D AL_TESTS=${AL_TESTS:-ON}
+  -D AL_PLUGINS=${AL_PLUGINS:-ON}
+  # Build documentation
+  -D AL_HLI_DOCS=${AL_HLI_DOCS:-ON}
+  # Work around Boost linker issues on 2020b toolchain
+  -D Boost_NO_BOOST_CMAKE=${Boost_NO_BOOST_CMAKE:-ON}
 )
 # Note: compilers are set as environment variables in the Bamboo config
 cmake -B build "${CMAKE_ARGS[@]}"
